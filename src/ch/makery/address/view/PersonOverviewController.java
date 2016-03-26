2dfce59dd1d8f8dbb48a38470b380d94863ea263
Ricardo Cotienschi <ricardocotienschi@live.com>
2016-03-26 15:29:34 -0300

    1 - Inserindo, Alterando e Deletando dados.

--- src/ch/makery/address/view/PersonOverviewController.java
+++ src/ch/makery/address/view/PersonOverviewController.java
@@ -60,5 +60,32 @@ public class PersonOverviewController {
         // Adiciona os dados da observable list na tabela
         personTable.setItems(mainApp.getPersonData());
     }
-  
+    
+    /**
+     * PReenche todos os campos de texto para mostrar detalhes sobre a pessoa.
+     * Se a pessoa especificada for null, todos os campos de texto são limpos.
+     * 
+     * @param person a pessoa ou null
+     */
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            // Preenche as labels com informações do objeto person.
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+
+            // TODO: Nós precisamos de uma maneira de converter o aniversário em um String! 
+            // birthdayLabel.setText(...);
+        } else {
+            // Person é null, remove todo o texto.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
+    }    
 }
